435ce17b975d07cf95d0f4d6fae43da05cce4365
TheMoira <48563226+TheMoira@users.noreply.github.com>
2019-08-03 18:10:44 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+print("Moira")
+print("*" * 10)
+price = 10
+print(price)
+is_published = False
+
+name = input('Gimme name, man ')
+print('U gonna get killed, ' + name)
+print('But not today')
+
+
+
